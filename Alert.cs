d5df1241a3ba456317eb6149c185fe9424e74a03
PsykoDev <45910905+PsykoDev@users.noreply.github.com>
2022-10-19 16:02:35 +0200

    Add files via upload

---
+++ Alert.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XSS_Tester
+{
+    static class Alert
+    {
+        public static IAlert WaitGetAlert(this IWebDriver driver, int waitTimeInSeconds = 5)
+        {
+            IAlert alert = null;
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitTimeInSeconds));
+
+            try
+            {
+                IAlert? alert1 = wait.Until(d =>
+                                {
+                                    try
+                                    {
+                                        return driver.SwitchTo().Alert();
+                                    }
+                                    catch (NoAlertPresentException)
+                                    {
+                                        return null;
+                                    }
+                                });
+                alert = alert1;
+            }
+            catch (WebDriverTimeoutException) { alert = null; }
+
+            return alert;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Net;
+
+namespace XSS_Tester
+{
+    internal class Program
+    {
+
+        static string uriTest = "https://xss-quiz.int21h.jp/";
+
+        static void Main(string[] args)
+        {
+            foreach( string arg in args)
+            {
+                uriTest = arg;
+            }
+
+            if(uriTest != string.Empty)
+            {
+                TestStart(uriTest);
+            }
+        }
+
+        static async void TestStart(string uriTest)
+        {
+            string[] lines = File.ReadAllLines("wordlist/xss-payload-list.txt");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine("Test: "+uriTest + line);
+                await RequestTest(uriTest + line);
+                
+            }
+
+        }
+
+        static Task RequestTest(string v)
+        {
+            try
+            {
+
+                ChromeOptions options = new ChromeOptions();
+                options.PageLoadStrategy = PageLoadStrategy.Normal;
+                ChromeDriverService driverService = ChromeDriverService.CreateDefaultService();
+                driverService.HideCommandPromptWindow = true;
+                driverService.EnableVerboseLogging = false;
+                driverService.SuppressInitialDiagnosticInformation = true;
+                driverService.EnableAppendLog = false;
+
+                ChromeDriver driver = new ChromeDriver(driverService, options);
+                driver.Navigate().GoToUrl(v);
+
+                var alert = Alert.WaitGetAlert(driver, 0);
+                if (alert != null)
+                {
+                    Console.WriteLine("!!!!Found: " +v);
+                    Console.WriteLine(alert.Text);
+                }
+
+                driver.Quit();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("ERROR:\n"+e);
+            }
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
